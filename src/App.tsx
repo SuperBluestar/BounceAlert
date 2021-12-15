d3a4f52073b9479ebd2f6563fc8fedaec3b229c7
SuperBluestar <alexander116gm@hotmail.com>
2021-12-16 08:43:00 +0900

    add meta data

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import LandingPage from './pages/LandingPage';
+import { Helmet } from "react-helmet";
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -13,6 +14,12 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Bounce Alerts</title>
+        <link rel="canonical" href="https://bouncealerts.com/" />
+        <meta content="Developed by superbluestar. You can find me live:.cid.4ed5b0d404ec64fc in skype or contact me via alexander116gm@gmail.com"/>
+      </Helmet>
       <Routes>
         <Route path="" element={<Homepage />} />
         <Route path="test" element={<LandingPage />} />
